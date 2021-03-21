ba1403f6d34a9487e4a4d8132d700184e1156be3
Rubin99 <rubinshrestha14@gmail.com>
2021-03-21 08:42:11 +0545

    AddTaskActivity FAB

--- app/src/main/java/com/example/todoappcw/MainActivity.java
+++ app/src/main/java/com/example/todoappcw/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.todoappcw;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.todoappcw.data.AppDatabase;
 import com.example.todoappcw.data.Repository;
 import com.example.todoappcw.data.Task;
 import com.example.todoappcw.data.TodoDao;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private  TaskAdapter adapter;
     Repository repository;
+    private FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
         List<Task> tasks = repository.getAllTask();
         adapter = new TaskAdapter(tasks);
         recyclerView.setAdapter(adapter);
+
+        addButton = findViewById(R.id.add_btn);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
